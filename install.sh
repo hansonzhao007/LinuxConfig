392626d5fb70f7e76b915d350daaafcecdd3e735
Xingsheng Zhao <zhxshseu@gmail.com>
2018-05-28 11:06:24 -0500

    new

---
+++ install.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+put ()
+{
+    if [[ -f "${2}" ]]; then
+        if [[ -x $(which colordiff 2>/dev/null) ]]; then
+            colordiff "${2}" "${1}"
+        elif [[ -x $(which diff 2>/dev/null) ]]; then
+            diff "${2}" "${1}"
+        fi
+    fi
+
+    if [[ -x $(which rsync 2>/dev/null) ]]; then
+            rsync -pc "${1}" "${2}"
+        else
+            cp "${1}" "${2}"
+    fi
+}
+
+# Whether mac or other linux
+if [[ -f "/etc/os-release" ]];then
+    MAC=0
+    echo "linux"
+else
+    MAC=1
+    echo "Mac"
+fi
+
+# normal .xx files
+#echo "install all config files into your HOME."
+DEST=${1:-${HOME}}
+echo "[DEST=${DEST}]"
+
+# .aliases, .vimrc ...
+for cf in $(cat ./config.list); do
+    put "config/$cf" "${DEST}/.$cf"
+done
+
+# install vim plugin 
+mkdir "~/.vim"
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim -c ":PluginInstall"
