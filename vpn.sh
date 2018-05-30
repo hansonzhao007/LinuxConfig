49b7626bff001cf58a2e71bd0e5f285b7af5adb3
Xingsheng Zhao <zhxshseu@gmail.com>
2018-05-29 19:04:45 -0500

    new

--- vpn.sh
+++ vpn.sh
@@ -1,1 +1,18 @@
-ssh -2fqnNT -D 9999 "fbhead"
+# ref: https://goo.gl/ugBYgU
+
+# ==================
+#  1. 本地机器配置
+#  a) 在本地创建 ssh key，然后在 server 端添加该 key。比如 hanson user 的 key
+#  b) 在本地设置 ssh 转发 tunnel
+# 
+#  -D: Tells SSH that we want a SOCKS tunnel on the specified port number (you can choose a number between 1025-65536)
+#  -f: Forks the process to the background
+#  -C: Compresses the data before sending it
+#  -q: Uses quiet mode
+#  -N: Tells SSH that no command will be sent once the tunnel is up
+# ==================
+ssh -2fqnNT -D 9999 hanson@141.217.24.182
+
+# ==================
+#  2. 在 firefox 里面设置 proxy 
+# ==================
