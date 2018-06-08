0323448c5a3da9bfcf3eb3b27bf4f739ede3e77f
Xingsheng Zhao <zhxshseu@gmail.com>
2018-06-08 10:25:18 -0500

    add bash-it

--- install.sh
+++ install.sh
@@ -44,3 +44,8 @@ rsync -rpc bin/ ${DEST}/Program/usr/bin
 
 echo "source .aliases " >> ~/.bashrc
 source ~/.bashrc
+
+
+# Install bash-it
+git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+~/.bash_it/install.sh
