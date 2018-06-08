09b08d909938a10179bc5228c7acdebfc6ed0dec
Xingsheng Zhao <zhxshseu@gmail.com>
2018-06-08 11:14:15 -0500

    new

--- install.sh
+++ install.sh
@@ -47,6 +47,7 @@ rsync -rpc bin/ ${DEST}/Program/usr/bin
 git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
 ~/.bash_it/install.sh
 
-echo "source ~/.aliases " >> ~/.bashrc
 echo "export BASH_IT_THEME='nwinkler'" >> ~/.bashrc
+echo "source ~/.aliases " >> ~/.bashrc
+echo 'source "$BASH_IT"/bash_it.sh' >> ~/.bashrc
 source ~/.bashrc
